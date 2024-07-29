6036343917643d1747954a193218c045d5ec1aca
Sunay Jain <sunay1j@gmail.com>
2024-07-29 09:26:26 -0400

    new drive updates from yesterday

--- src/main/java/frc/robot/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/drivetrain/Drivetrain.java
@@ -13,6 +13,7 @@ import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.wpilibj.Encoder;
 
+
 public class Drivetrain extends SubsystemBase {
     private CANSparkMax leftLeader = new CANSparkMax(0, kBrushless);
     private CANSparkMax leftFollower = new CANSparkMax(1, kBrushless);
@@ -40,17 +41,27 @@ public class Drivetrain extends SubsystemBase {
     }
 
     /**
+     * command for driving
+     * @param leftSpeed
+     * @param rightSpeed
+     * @return
+     */
+    public Command driveCommand(double leftSpeed, double rightSpeed){
+        return Commands.run(() -> drive(leftSpeed, rightSpeed));
+    }
+
+    /**
      * updates voltage based on PID in order to drive a certain distance
      * @return voltage of the motors
      */
     public double driveDistance(Measure<Distance> meters) {
         double voltage = 0;
-         
+        double encoderValue = leftEncoder.get() + rightEncoder.get()/2;
+        voltage = pidControllerTranslation.calculate(encoderValue, Meters.convertFrom(meters, Meters));
         // code to calculate voltage that needs to be applied-
         // in order to move a certain distance
         // hint: use PID
-
-        return voltage;
+        drive(Math.sqrt(voltage), Math.sqrt(voltage));
     }
 
 
