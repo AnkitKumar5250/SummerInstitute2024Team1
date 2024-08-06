88be9b1f24674b218d9ba5acf3bda9b685377211
Sunay Jain <sunay1j@gmail.com>
2024-08-06 09:14:23 -0400

    converted intake pid to use angle as start/stop point

--- src/main/java/frc/robot/intake/IntakeConstants.java
+++ src/main/java/frc/robot/intake/IntakeConstants.java
@@ -1,8 +1,12 @@
 package frc.robot.intake;
 
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import static edu.wpi.first.units.Units.Degrees;
+
 public class IntakeConstants {
     // encoder constants for extend() and retract() respectively
-    public static final double stopPoint = 0; 
+    public static final double stopPoint = 90; 
     public static final double startPoint = 0;
     public static final double intakeP = 0;
     public static final double intakeI = 0;
